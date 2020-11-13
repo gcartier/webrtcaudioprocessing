394144779b6cfe53563017b673e31b2c40247420
Guillaume <gucartier@gmail.com>
2020-11-13 10:35:03 -0500

    Reenable logging-severity property

--- plugin/src/gstwebrtcaudioprocessor.cpp
+++ plugin/src/gstwebrtcaudioprocessor.cpp
@@ -81,8 +81,14 @@
   #define SHARED_PUBLIC __attribute__ ((visibility ("default")))
 #endif
 
+// should go in a webrtc.h
 #define kMaxDataSizeSamples 7680
-extern "C" SHARED_PUBLIC int  ap_setup(int, bool, bool, bool);
+#define LS_NONE 0
+#define LS_ERROR 1
+#define LS_WARNING 2
+#define LS_INFO 3
+#define LS_VERBOSE 4
+extern "C" SHARED_PUBLIC int  ap_setup(int, bool, bool, int);
 extern "C" SHARED_PUBLIC void ap_delete();
 extern "C" SHARED_PUBLIC void ap_delay(int);
 extern "C" SHARED_PUBLIC int  ap_process_reverse(int, int, int16_t*);
@@ -124,8 +130,7 @@ GST_STATIC_PAD_TEMPLATE ("src",
         "channels = (int) [1, MAX]")
     );
 
-#ifdef _WAIT
-typedef rtc::LoggingSeverity GstWebrtcAudioProcessingLoggingSeverity;
+typedef int GstWebrtcAudioProcessingLoggingSeverity;
 #define GST_TYPE_WEBRTC_LOGGING_SEVERITY \
     (gst_webrtc_logging_severity_get_type ())
 static GType
@@ -133,11 +138,11 @@ gst_webrtc_logging_severity_get_type (void)
 {
   static GType logging_severity_type = 0;
   static const GEnumValue severity_types[] = {
-    {rtc::LS_VERBOSE, "Verbose logging", "verbose"},
-    {rtc::LS_INFO, "Info logging", "info"},
-    {rtc::LS_WARNING, "Warning logging", "warning"},
-    {rtc::LS_ERROR, "Error logging", "error"},
-    {rtc::LS_NONE, "No logging", "none"},
+    {LS_VERBOSE, "Verbose logging", "verbose"},
+    {LS_INFO, "Info logging", "info"},
+    {LS_WARNING, "Warning logging", "warning"},
+    {LS_ERROR, "Error logging", "error"},
+    {LS_NONE, "No logging", "none"},
     {0, NULL, NULL}
   };
 
@@ -148,6 +153,7 @@ gst_webrtc_logging_severity_get_type (void)
   return logging_severity_type;
 }
 
+#ifdef _WAIT
 typedef webrtc::AudioProcessing::Config::NoiseSuppression::Level GstWebrtcAudioProcessingNoiseSuppressionLevel;
 #define GST_TYPE_WEBRTC_NOISE_SUPPRESSION_LEVEL \
     (gst_webrtc_noise_suppression_level_get_type ())
@@ -234,9 +240,7 @@ struct _GstWebrtcAudioProcessor
   GstWebrtcAudioProbe *probe;
 
   /* Properties */
-#ifdef _WAIT
-  rtc::LoggingSeverity logging_severity;
-#endif
+  int logging_severity;
   int processing_rate;
   gboolean high_pass_filter;
   gboolean echo_cancel;
@@ -562,7 +566,7 @@ gst_webrtc_audio_processor_start (GstBaseTransform * btrans)
 #else
   gint err = 0;
   GST_OBJECT_LOCK (self);
-  err = ap_setup(32000, true, true, false);
+  err = ap_setup(32000, true, true, self->logging_severity);
 #endif
 
   if (err < 0)
@@ -717,12 +721,10 @@ gst_webrtc_audio_processor_set_property (GObject * object,
       g_free (self->probe_name);
       self->probe_name = g_value_dup_string (value);
       break;
-#ifdef _WAIT
     case PROP_LOGGING_SEVERITY:
       self->logging_severity =
           (GstWebrtcAudioProcessingLoggingSeverity) g_value_get_enum (value);
       break;
-#endif
     case PROP_PROCESSING_RATE:
       self->processing_rate = g_value_get_int (value);
       break;
@@ -786,11 +788,9 @@ gst_webrtc_audio_processor_get_property (GObject * object,
     case PROP_PROBE:
       g_value_set_string (value, self->probe_name);
       break;
-#ifdef _WAIT
     case PROP_LOGGING_SEVERITY:
       g_value_set_enum (value, self->logging_severity);
       break;
-#endif
     case PROP_PROCESSING_RATE:
       g_value_set_int (value, self->processing_rate);
       break;
@@ -901,15 +901,13 @@ gst_webrtc_audio_processor_class_init (GstWebrtcAudioProcessorClass * klass)
           (GParamFlags) (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS |
               G_PARAM_CONSTRUCT)));
 
-#ifdef _WAIT
   g_object_class_install_property (gobject_class,
       PROP_LOGGING_SEVERITY,
       g_param_spec_enum ("logging-severity", "Logging Severity",
           "Controls the amount of logging.", GST_TYPE_WEBRTC_LOGGING_SEVERITY,
-          rtc::LS_WARNING,
+          LS_WARNING,
           (GParamFlags) (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS |
               G_PARAM_CONSTRUCT)));
-#endif
 
   g_object_class_install_property (gobject_class,
       PROP_PROCESSING_RATE,
